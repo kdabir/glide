bc23a5997798ac20848cef90240c2c53f182e9ac
Kunal Dabir <kunal.dabir@gmail.com>
2013-05-05 23:05:38 +0530

    support the generation of cron xml file

---
+++ runner/src/test/groovy/glide/generators/CronXmlGeneratorTest.groovy
@@ -0,0 +1,25 @@
+package glide.generators
+
+/**
+ * 
+ */
+class CronXmlGeneratorTest extends GroovyTestCase {
+
+    def config = new ConfigSlurper().parse("""
+    cron {
+         entries = [
+            [url:"test", description:"test cron", scehdule:"every time"],
+            [url:"other/url", schedule:"every now and then"]
+        ]
+    }
+    """)
+
+    void testGenerate() {
+        def cronXmlString = new CronXmlGenerator().generate(config)
+        def cronentries  = new XmlSlurper().parseText(cronXmlString)
+        assert cronentries.cron[0].url == "test"
+        assert cronentries.cron[1].url == "other/url"
+
+    }
+
+}
