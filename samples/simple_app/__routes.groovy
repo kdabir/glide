2f9fd993f33a1cb4d1060917e7e9bc73598359c5
Kunal Dabir <kunal.dabir@gmail.com>
2013-05-24 21:46:24 +0530

    update the sample app

--- samples/simple_app/__routes.groovy
+++ samples/simple_app/__routes.groovy
@@ -1,4 +1,4 @@
 get "/hello/@name", forward : "/hello.groovy?name=@name"
-get "/user/me", forward : "/user/welcome.groovy"
 get "/hello", forward : "/hello.groovy?name=guest"
+get "/user/me", forward : "/user/welcome.groovy"
 get "/", forward: "/index.html"
