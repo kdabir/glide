ae61d6a48321f333730e3a7097ee8ab7394e8f67
Kunal Dabir <kunal.dabir@gmail.com>
2016-07-07 10:32:57 +0530

    accept env

--- glide-gradle-plugin/src/glide/config/ChainedConfigLoader.groovy
+++ glide-gradle-plugin/src/glide/config/ChainedConfigLoader.groovy
@@ -7,14 +7,16 @@ class ChainedConfigLoader {
 
     final Map context
     final ConfigObject config
+    final String defaultEnv
 
-    ChainedConfigLoader(final Map context = [:], ConfigObject initialConfig = new ConfigObject()) {
+    ChainedConfigLoader(final Map context = [:], ConfigObject initialConfig = new ConfigObject(), String defaultEnv = null) {
+        this.defaultEnv = defaultEnv
         this.context = Collections.unmodifiableMap(context)
         this.config = initialConfig
     }
 
-    ChainedConfigLoader load(String configString) { // wish we could pass something like union type A | B
-        final ConfigSlurper slurper = new ConfigSlurper()
+    ChainedConfigLoader load(String configString, String env = null) { // wish we could pass something like union type A | B
+        final ConfigSlurper slurper = new ConfigSlurper(env?:defaultEnv)
         slurper.setBinding(context + config)
 
         ConfigObject latest = slurper.parse(configString)
